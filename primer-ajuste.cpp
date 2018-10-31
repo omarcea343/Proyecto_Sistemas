deebdd6eab2fdfd35a20fca3760dff2138acf5c5
Cuco1897 <32716183+Cuco1897@users.noreply.github.com>
2018-10-31 13:48:16 -0600

    Update primer-ajuste.cpp

--- primer-ajuste.cpp
+++ primer-ajuste.cpp
@@ -1,47 +1,67 @@
-//Metodo de Asignacion de memoria "Primer Ajuste"  //
-//Hecho por: J. Refugio Muñoz Serna  //
-
+// C++ implementation mejor ajuste 
+#include<cstring>
 #include <iostream>
+using namespace std; 
 
-using namespace std;
+// Function to allocate memory to blocks as per Best fit 
+// algorithm 
+void bestFit(int blockSize[], int m, int processSize[], int n) 
+{ 
+	// Stores block id of the block allocated to a 
+	// process 
+	int allocation[n]; 
 
-struct proceso {
-	int ID;
-	int tam;
-	int cont ;
-};
+	// inicializa los bloques de memoria como libres
+	memset(allocation, -1, sizeof(allocation)); 
 
-void colocacion ();
-void remplazo ();
-void mostrar (int tam, struct proceso);
+	// Busca cual bloque esta disponible para usar  y de acuerdo a su tamaño decide si lo usa o no
+	for (int i=0; i<n; i++) 
+	{ 
+		// Busca el mejor bloque en cual guardar que es en el que sobre menos espacio 
+		int bestIdx = -1; 
+		for (int j=0; j<m; j++) 
+		{ 
+			if (blockSize[j] >= processSize[i]) 
+			{ 
+				if (bestIdx == -1) 
+					bestIdx = j; 
+				else if (blockSize[bestIdx] > blockSize[j]) 
+					bestIdx = j; 
+			} 
+		} 
+
+		// Aqui guarda el proceso en el bloque 
+		if (bestIdx != -1) 
+		{ 
+			// Guarda el proceso p[i] en el bloque en posicion j  
+			allocation[i] = bestIdx; 
+
+			// Quita  los espacios de memoria ya utilizados 
+			blockSize[bestIdx] -= processSize[i]; 
+		} 
+	} 
+
+	cout << "\nProcess No.\tProcess Size\tBlock no.\n"; 
+	for (int i = 0; i < n; i++) 
+	{ 
+		cout << " " << i+1 << "\t\t" << processSize[i] << "\t\t"; 
+		if (allocation[i] != -1) 
+			cout << allocation[i] + 1; 
+		else
+			cout << "Not Allocated"; 
+		cout << endl; 
+	} 
+} 
 
 
-int main (){
-	proceso dato;
-	
-	int tam;
-	cout<<"Tamanio de la memoria (cantidad de espacios): "<<endl;
-	cin>>tam;
-	
-	mostrar(tam, dato);
-	
-	return 0;
-}
+int main() 
+{ 
+	int blockSize[] = {100, 500, 200, 300, 600}; 
+	int processSize[] = {212, 417, 112, 426}; 
+	int m = sizeof(blockSize)/sizeof(blockSize[0]); 
+	int n = sizeof(processSize)/sizeof(processSize[0]); 
 
-void mostrar(int tam, struct proceso ){
-	
-	proceso dato;
-	
-	for(int i=0; i<tam; i++){	
-	cout<<"["; cout<<", ",dato.tam; cout<<", ]", dato.cont;  //[ID, Tamaño, Cuanto];
-	}
-}
-void colocacion (){
-	
-	
-}
+	bestFit(blockSize, m, processSize, n); 
 
-void remplazo(){
-	
-	
-}
+	return 0 ; 
+} 
