1def195c59d6e2cbf5d3e98b9f26957102de4722
Omar Zuñiga <miguel_omar30@hotmail.com>
2018-11-08 09:33:51 -0600

    Update memoria.cpp

--- memoria.cpp
+++ memoria.cpp
@@ -7,11 +7,11 @@
 using namespace std;
 
 void primerAjuste(int p[], int po[], int m[], int n, int j);
-void mejorAjuste(int y, int n, int z, int m[], int temp1, int temp, int po[], int flag, int k, int p[],int i, int j);
-void peorAjuste(int y, int n, int z, int m[], int temp1, int temp, int po[], int flag, int k, int p[],int i, int j);
+void mejorAjuste(int p[], int po[], int m[], int n, int j);
+void peorAjuste(int p[], int po[], int m[], int n, int j);
 
 int main(){
-	int c, j, k, n, l, m[10], p[10], po[20], flag, z, y, temp, temp1;//Declaracion de variables
+	int c, j, n, m[10], p[10], po[20];//Declaracion de variables
 	
 	srand(time(NULL));//Numeros aleatorios
 	
@@ -45,9 +45,9 @@ int main(){
     	case 1:
 			primerAjuste(p,po,m,n,j);//Funcion Primer Ajuste
     	case 2:
-        	mejorAjuste(y,n,z,m,temp1,temp,po,flag,k,p,i,j);//Funcion Mejor Ajuste
+        	mejorAjuste(p,po,m,n,j);//Funcion Mejor Ajuste
         case 3:
-        	peorAjuste(y,n,z,m,temp1,temp,po,flag,k,p,i,j);//Funcion Peor Ajuste
+        	peorAjuste(p,po,m,n,j);//Funcion Peor Ajuste
     }
 
     return 0;
@@ -74,64 +74,69 @@ void primerAjuste(int p[], int po[], int m[], int n, int j){
     }
 }
 
-void mejorAjuste(int y, int n, int z, int m[], int temp1, int temp, int po[], int flag, int k, int p[],int i, int j){
-	for(y=1;y<=n;y++){
-	    for(z=y;z<=n;z++){
-        	if(m[y]>m[z]){
-        		temp=m[y];
-          		m[y]=m[z];
-          		m[z]=temp;
-          		temp1=po[y]; 
-          		po[y]=po[z];
-          		po[z]=temp1;            
+void mejorAjuste(int p[], int po[], int m[], int n, int j){
+	int temp, temp1, flag;
+	
+	for(int  y = 1; y <= n; y++){
+	    for(int z = y; z <= n; z++){
+        	if(m[y] > m[z]){
+        		temp = m[y];
+          		m[y] = m[z];
+          		m[z] = temp;
+          		temp1 = po[y]; 
+          		po[y] = po[z];
+          		po[z] = temp1;            
           	}                 
         }             
     }
     
-    for(i=1;i<=j;i++){
-    	flag=1;
-        for(k=1;k<=n;k++){
-          	if(p[i]<=m[k]){
+    for(int i = 1; i <= j; i++){
+    	flag = 1;
+        for(int k = 1; k <= n; k++){
+          	if(p[i] <= m[k]){
              	cout<<"\nprocess "<<i<<" whose memory size is "<<p[i]<<"KB allocated at memory partition:\t"<<po[k];
-             	m[k]=m[k]-p[i];
+             	m[k] = m[k] - p[i];
              	break;           
           	}
           	else{
-            flag++;  
+	            flag++;  
           	}
       	}   
-      	if(flag>n){
+      	if(flag > n){
         	cout<<"\nprocess "<<i<<" whose memory size is "<<p[i]<<"KB can't be allocated";       
       	}           
     }
 }
 
-void peorAjuste(int y, int n, int z, int m[], int temp1, int temp, int po[], int flag, int k, int p[],int i, int j){
-	for(y=1;y<=n;y++){
-        for(z=y;z<=n;z++){
-          	if(m[y]<m[z]){
-          		temp=m[y];
-          		m[y]=m[z];
-          		m[z]=temp;
-          		temp1=po[y]; 
-          		po[y]=po[z];
-          		po[z]=temp1;            
+void peorAjuste(int p[], int po[], int m[], int n, int j){
+	int temp, temp1, flag;
+	
+	for(int y = 1; y <= n; y++){
+        for(int z = y; z <= n; z++){
+          	if(m[y] < m[z]){
+          		temp = m[y];
+          		m[y] = m[z];
+          		m[z] = temp;
+          		temp1 = po[y]; 
+          		po[y] = po[z];
+          		po[z] = temp1;            
           	}                 
         }             
     }
-    for(i=1;i<=j;i++){
-        flag=1;
-        for(k=1;k<=n;k++){
-          	if(p[i]<=m[k]){
+    
+    for(int  i = 1; i <= j; i++){
+        flag = 1;
+        for(int k = 1; k <= n; k++){
+          	if(p[i] <= m[k]){
              	cout<<"\nprocess "<<i<<" whose memory size is "<<p[i]<<"KB allocated at memory partition:\t"<<po[k];
-             	m[k]=m[k]-p[i];
+             	m[k] = m[k] - p[i];
              	break;           
           	}
           	else{
             	flag++;  
           	}
       	}   
-      	if(flag>n){
+      	if(flag > n){
         	cout<<"\nprocess "<<i<<" whose memory size is "<<p[i]<<"KB can't be allocated";       
       	}           
     }
